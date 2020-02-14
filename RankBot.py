be9c5d0b409edcfc66dfb58ff4e6075064418212
SaranshPK <saranshis@hotmail.com>
2020-02-13 22:36:55 -0800

    Updated Comments for readability

--- RankBot.py
+++ RankBot.py
@@ -5,10 +5,9 @@ import time
 import io
 
 Regions = ["na1","la1","la2"]
-
-Host = "127.0.0.1"
 Port = "10011"
 
+# server group id corresponding to each rank
 servergroupsNA = {
     "IRON": 225,
     "BRONZE": 159,
@@ -20,7 +19,6 @@ servergroupsNA = {
     "GRANDMASTER": 227,
     "CHALLENGER":228
 }
-
 servergroupsLAN = {
     "IRON": 230,
     "BRONZE": 201,
@@ -33,27 +31,33 @@ servergroupsLAN = {
     "CHALLENGER":207
 }
 
+# load authentication variables
 with open("auth.txt") as authfile:
     authVars = json.load(authfile)
     APIKey = authVars['APIKey']
     password = authVars['password']
     Host = authVars["host"]
-    
+
+# get summoner data from their name and region
 def requestSummonerData(region, summonerName):
     URL = "https://" + region + ".api.riotgames.com/lol/summoner/v4/summoners/by-name/" + summonerName + "?api_key=" + APIKey
     response = requests.get(URL)
     return response.json()
 
+# get a summoners ranked data based on their ID and region
 def requestRankedData(region, ID):
     URL = "https://"+region+".api.riotgames.com/lol/league/v4/entries/by-summoner/" + ID + "?api_key=" + APIKey
     response = requests.get(URL)
     return response.json()
 
+# assign groups to a user identified by their cldbid (client database ID)
 def assignGroup(cldbid,telnet):
 
+    # loads user json
     with open('userdata.txt') as userfile:
         users = json.load(userfile)
 
+    # if the user doesnt exist prompts a message to register the user
     try:
         user = users[cldbid]
     except KeyError:
@@ -61,12 +65,17 @@ def assignGroup(cldbid,telnet):
         telnet.write(command.encode('ascii'))
         return
 
+    # gets a user's ranked info and assigns them the correct rank
     summonerID = user['summonerId']
 
+    # some users dont play league
     if summonerID:
+
         region = Regions[int(user['region'])]
         rankeddata = requestRankedData(region, summonerID)
 
+        # only give ranks to people who have ranked data
+        # some people are unranked
         try:
             rank = rankeddata[0]['tier']
         except KeyError:
@@ -74,14 +83,18 @@ def assignGroup(cldbid,telnet):
         except IndexError:
             return
 
+        # get the servergroup corresponding to thier rank
         if region == "na1":
             servergroup = str(servergroupsNA[rank])
         else:
             servergroup = str(servergroupsLAN[rank])
 
+        # teamspeak query command to give a rank to a client
         addgroupcommand = "servergroupaddclient sgid=" + servergroup + " cldbid=" + cldbid + "\n"
         telnet.write(addgroupcommand.encode('ascii'))
 
+# start of program connect the bot to the teamspeak server
+# and set up the bot to receive server and text updates
 telnet = telnetlib.Telnet()
 telnet.open(Host, Port)
 telnet.write(("login serveradmin " + password + "\n").encode('ascii'))
@@ -90,31 +103,46 @@ telnet.write("clientupdate client_nickname=RankBot\n".encode('ascii'))
 telnet.write("servernotifyregister event=server\n".encode('ascii'))
 telnet.write("servernotifyregister event=textprivate\n".encode('ascii'))
 
+# counter
 i = 0
+
+# always listening to the teamspeak notifcations
 while 1:
+
+    # read and filter server output
     readData = telnet.read_very_eager()
     readData = readData.decode("utf-8")
     lines = readData.splitlines()
     lines = filter(None,lines)
     for line in lines:
+
+        # logs server messages
         print(line)
+
+        # on user connection
         if line.startswith("notifycliententerview"):
 
+            # get their cluid (client unique id)
             cluidstart = line.index("client_unique_identifier")+25
             cluidend = line.index(" ",cluidstart)
             cluid = line[cluidstart:cluidend]
 
+            # request their cldbid (client database id)
             telnet.write(("clientgetdbidfromuid cluid=" + cluid + "\n").encode('ascii'))
 
+        # upon receiving a clients cldbid
         if line.startswith("cluid"):
             
             cldbidstart = line.index("cldbid")+7
             cldbid = line[cldbidstart:]
 
+            # assign server group corresponding to league rank
             assignGroup(cldbid, telnet)
 
+        # upon receiving !register command
         if line.startswith("notifytextmessage targetmode=1 msg=!register"):
 
+            # parse data from command
             startindex = line.index("!register")+11
             endindex = line.index(" ", startindex)
             registerdata = line[startindex:endindex]
@@ -122,20 +150,27 @@ while 1:
             cldbid = splitinfo[0]
             region = splitinfo[1]
             name = splitinfo[2].replace("\\s", "%20")
+
+            # request league id from name and region
             ID = requestSummonerData(Regions[int(region)], name)['id']
 
+            # get user list
             with open('userdata.txt') as userfile:
                 users = json.load(userfile)
             users[cldbid]={"summonerId": ID, "region": region}
 
+            # add user to list
             with open('userdata.txt', 'w') as userfile:
                 json.dump(users, userfile)
             
+            # assign server group corresponding to league rank
             assignGroup(cldbid, telnet)
 
+    # sleep for half a second before each read
     time.sleep(0.5)
+
+    # send a useless command so teamspeak connection doesnt time out
     if i == 300:
         telnet.write("whoami\n".encode('ascii'))
         i=0
     i = i+1
-telnet.close()
